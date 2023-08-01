b0f4f2806cf4ed0dca8301376d5a0b58a04e9220
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-08-01 18:30:15 +0530

    trying hover

--- app/page.js
+++ app/page.js
@@ -1,29 +1,57 @@
 'use client'
-import Image from 'next/image'
-import next from "../public/next.svg"
-import vercel from "../public/vercel.svg"
-import {motion} from "framer-motion"
-import { useState } from 'react'
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
+import galaxy1 from "../public/galaxy1.jpeg";
+import galaxy2 from "../public/galaxy2.jpeg";
+
 export default function Home() {
-  const[isHovered,setIsHovered] = useState(false);
-  const handleHover=()=>{
+  const [isHovered, setIsHovered] = useState(false);
+  const handleHover = () => {
     setIsHovered(!isHovered);
   };
+
   return (
-    <div onMouseEnter={handleHover} onMouseLeave={handleHover} className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-    <motion.img style={{opacity:isHovered?0:1}} className="w-auto h-auto rounded-t-xl" src={next} alt="Next Logo"/>
-    <motion.img style={{opacity:isHovered?1:0}} className="w-auto h-auto rounded-t-xl" src={vercel} alt="Vercel Logo"/>
-    <div className="p-4 md:p-5">
-      <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-        My Card
-      </h3>
-      <p className="mt-1 text-gray-800 dark:text-gray-400">
-        Hover to switch images
-      </p>
-      <a className="mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" href="#">
-        Go somewhere
-      </a>
-    </div>
-  </div> 
-  )
+    <motion.div
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+      className={`flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] ${
+        isHovered ? "space-x-2 rotate-open-book" : ""
+      } card-container`}
+    >
+      <div className="flex space-x-2">
+        <Image
+          style={{ opacity: isHovered ? 0 : 1 }}
+          className="w-auto h-auto transform-galaxy-card-left"
+          width={336}
+          height={336}
+          src={galaxy1}
+          alt="Galaxy 1 "
+        />
+        <Image
+          style={{ opacity: isHovered ? 1 : 0 }}
+          className="w-auto h-auto transform-galaxy-card-right"
+          width={336}
+          height={336}
+          src={galaxy2}
+          alt="Galaxy 2"
+        />
+      </div>
+      <div className="p-4 md:p-5">
+        <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+          My Card
+        </h3>
+        <p className="mt-1 text-gray-800 dark:text-gray-400">
+          Hover to switch images
+        </p>
+        <a
+          className="mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
+          href="#"
+        >
+          Go somewhere
+        </a>
+      </div>
+    </motion.div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      transformOrigin: {
+        'galaxy-card-left': 'right',
+        'galaxy-card-right': 'left',
+      },
+      transition: {
+        'open-book': 'transform 0.5s ease',
+      },
     },
   },
   plugins: [],
