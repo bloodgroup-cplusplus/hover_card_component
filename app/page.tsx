16f2554d327a343ac0975ed638242fda4a014fea
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-08-01 12:07:41 +0000

    added page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
+    <div className="flex flex-col bg-white border shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]">
+  <img className="w-full h-auto rounded-t-xl" src="../docs/assets/img/500x300/img1.jpg" alt="Image Description">
+  <div className="p-4 md:p-5">
+    <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+      My Card
+    </h3>
+    <p className="mt-1 text-gray-800 dark:text-gray-400">
+      Hover to switch Images
+    </p>
+    <a className="mt-3 py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" href="#">
+      Go somewhere
+    </a>
+  </div>
+</div>
     
   )
 }
